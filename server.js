c7da22523414ed587019daeec00e30be527679ab
Berchie <agyemangnti@gmail.com>
2020-11-06 11:57:42 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const config = require('./utils/config');
 const express = require ('express');
-
 const app = express();
+const logger = require('./utils/logger');
 
+
+
+app.listen(config.PORT, ()=>{
+  logger.info(`Server is running on port ${config.PORT}`);
+})
