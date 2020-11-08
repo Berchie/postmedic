34b61aa47e5033bc74e406d4b7e33629e59b800c
Berchie <agyemangnti@gmail.com>
2020-11-08 00:40:17 +0000

    Add mongo db config

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const MONGODB_URI_CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
 
 module.exports = {
   MONGODB_URI,
-  PORT
-}
+  PORT,
+  MONGODB_URI_CONFIG,
+};
