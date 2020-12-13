98fe9c39032a952dc94d26f18c4ad75878ce6cbf
Berchie <agyemangnti@gmail.com>
2020-12-13 02:09:31 +0000

    Change root point

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './App.less';
 
 
-// import Home from './components/Home';
+import Home from './components/Home';
 // import Login from './components/Login';
-import PatientGrid from './components/PatientGrid';
+// import PatientGrid from './components/PatientGrid';
 // import Register from './components/Register';
 
 
 function App() {
   return (
     <div className="App">
-      <PatientGrid/>
+      <Home/>
     </div>
   );
 }
