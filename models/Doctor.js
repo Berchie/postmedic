bbd1f691fd6d02442bab7a817dc0efeab499478e
Berchie <agyemangnti@gmail.com>
2021-04-26 14:54:49 +0000

    add middleware to handle cascade delete of doctor from insitution collection

--- models/Doctor.js
+++ models/Doctor.js
@@ -1,13 +1,29 @@
 const mongoose = require("mongoose");
+const Institution = require("./Institution");
 
-const DocotrSchema = new mongoose.Schema({
-  name: {
-    firstname: { type: String, require: true },
-    lastname: { type: String, require: true },
+const DocotrSchema = new mongoose.Schema(
+  {
+    name: {
+      firstname: { type: String, require: true },
+      lastname: { type: String, require: true },
+    },
+    email: { type: String, lowercase: true, trim: true, required: true },
+    phone: { type: String, trim: true, required: true },
+    institution: { type: mongoose.Schema.Types.ObjectId, ref: "Institution" },
   },
-  email: { type: String, lowercase: true, trim: true, required: true },
-  phone: { type: String, trim: true, required: true },
-  institution: { type: mongoose.Schema.Types.ObjectId, ref: "Institution" },
-}, { autoIndex: false }, {timestamps: true});
+  { autoIndex: false },
+  { timestamps: true }
+);
 
+DocotrSchema.pre("remove", async function (next) {
+  try {
+    await Institution.findByIdAndUpdate(
+      { _id: this.institution }, //this key word is used here to refer to the document or DoctorSchema
+      { $pull: { doctors: this._id } }
+    );
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = mongoose.model("Doctor", DocotrSchema);
