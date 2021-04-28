ebf6ee38007e7b1b0499605d8052330a597916a9
Berchie <agyemangnti@gmail.com>
2021-04-28 19:08:51 +0000

    add middleware

--- models/ObstetricHistory.js
+++ models/ObstetricHistory.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
+const Patient = require("./Patient");
 
-const ObstetricHistorySchema = new mongoose.Schema({
-  numberOfPregnancies: { type: Number, min: 0, default: 0 },
-  numberOfBirth: { type: Number, min: 0, default: 0 },
-  numberOfAbortionSpontaneous: { type: Number, min: 0, default: 0 },
-  numberOfAbortionInduced: { type: Number, min: 0, default: 0 },
-  patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
-},{ autoIndex: false }, {timestamps: true});
+const ObstetricHistorySchema = new mongoose.Schema(
+  {
+    numberOfPregnancies: { type: Number, min: 0, default: 0 },
+    numberOfBirth: { type: Number, min: 0, default: 0 },
+    numberOfAbortionSpontaneous: { type: Number, min: 0, default: 0 },
+    numberOfAbortionInduced: { type: Number, min: 0, default: 0 },
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
+  },
+  { autoIndex: false },
+  { timestamps: true }
+);
 
+ObstetricHistorySchema.pre("remove", async function (next) {
+  try {
+    await Patient.remove({ _id: this.patient });
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = mongoose.model("ObstetricHistory", ObstetricHistorySchema);
