9421b5938f808c4e11ea254e5b1966b8166f2e05
Berchie <agyemangnti@gmail.com>
2021-04-28 10:41:49 +0000

    making changes to schema and middleware

--- models/Admission.js
+++ models/Admission.js
@@ -1,21 +1,29 @@
 const mongoose = require("mongoose");
 const Patient = require("./Patient");
 
-const AdmissionSchema = new mongoose.Schema({
-  dateOfAdmission: { require: true, type: Date },
-  dateOfDischarged: { require: true, type: Date },
-  durationOfStay: { type: Number},
-  dischargedDiagnosis: { require: true, type: String },
-  patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" }
-},{ autoIndex: false }, {timestamps: true});
+const AdmissionSchema = new mongoose.Schema(
+  {
+    admissionDate: { require: true, type: Date },
+    dischargedDate: { require: true, type: Date },
+    durationOfStay: { type: Number },
+    dischargedDiagnosis: { require: true, type: String },
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
+  },
+  { autoIndex: false },
+  { timestamps: true }
+);
 
-AdmissionSchema.pre('remove', function(next) {
-  Patient.update(
-      { admissions : this._id}, 
+AdmissionSchema.pre("remove", async function (next) {
+  try {
+    await Patient.update(
+      { _id: this.patient },
       { $pull: { admissions: this._id } },
-      { multi: true })  //if reference exists in multiple documents 
-  .exec();
-  next();
+      { multi: true } //if reference exists in multiple documents
+    ).exec(); 
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model("Admission", AdmissionSchema);
