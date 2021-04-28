d18740c22bc69435d0b4f535eadd6dd5136f108c
Berchie <agyemangnti@gmail.com>
2021-04-28 19:34:49 +0000

    add middleware

--- models/CurrentPregnancy.js
+++ models/CurrentPregnancy.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Patient = require("./Patient");
 
-const CurrentPregnancySchema = new mongoose.Schema({
-  edd: { type: Date },
-  ega: { type: Number, default: 0 },
-  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
-},{ autoIndex: false }, {timestamps: true});
+const CurrentPregnancySchema = new mongoose.Schema(
+  {
+    edd: { type: Date },
+    ega: { type: Number, default: 0 },
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
+  },
+  { autoIndex: false },
+  { timestamps: true }
+);
 
+CurrentPregnancySchema.pre("remove", async function (next) {
+  try {
+    await Patient.findByIdAndUpdate(
+      { _id: this.patient },
+      { $pull: { currentPregnancies: this._id } },
+      { multi: true }
+    );
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = mongoose.model("CurrentPregnancy", CurrentPregnancySchema);
