ea8244ecb92859359aa2ebfd1b03f1763b2f5228
Berchie <agyemangnti@gmail.com>
2021-05-11 09:19:27 +0000

    initial commit

---
+++ client/src/api/admissionAPI.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+//fetching admission for data table(datagrid)
+export const getAdmissionGrid = async () => {
+  try {
+    let res = await axios.get("/admission");
+    return res.data;
+  } catch (error) {
+    return `Something went wrong: ${error.message}`;
+  }
+};
