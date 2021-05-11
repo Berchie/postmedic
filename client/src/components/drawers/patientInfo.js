55bb3e69a200a2cf44d84be1db25fdcb671ac715
Berchie <agyemangnti@gmail.com>
2021-05-11 18:33:37 +0000

    initial commit

---
+++ client/src/components/drawers/patientInfo.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+import {Drawer, Divider} from 'antd'
+
+function patientInfo() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default patientInfo
