a41dee46a99cc53ac0474151cd0f3288dfca85c8
Berchie <agyemangnti@gmail.com>
2021-05-20 10:24:52 +0000

    initial commit

---
+++ client/src/components/drawers/tableAdmission.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+const columns = [
+  { id: 'admissiondate', label: 'Admission Date', minWidth: 100 },
+  { id: 'dischargedate', label: 'Discharged Date', minWidth: 100 },
+  { id: 'numberofdays', label: 'Number of Days', minWidth: 170, align: 'right'},
+  { id: 'dischargeddiagnosis', label: 'Discharged Diagnosis', minWidth: 170, },
+];
+
+function createData(admissiondate, dischargedate, numberofdays, dischargeddiagnosis) {
+  return { admissiondate, dischargedate, numberofdays, dischargeddiagnosis};
+}
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 300,
+  },
+});
+
+export default function TableAdmission({data}) {
+
+  const rows = [];
+  
+  try {
+    for (let i = 0; i < data.length; i++) {
+      rows.push(
+        createData(data[i].admissionDate, data[i].dischargedDate,data[i].durationOfStay,data[i].dischargedDiagnosis)
+      )
+      
+    }
+  } catch (error) {
+    console.log("Something went wrong!", error);
+  }
+
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
